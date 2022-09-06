136fa0cb806e24be8f13f50782e178e255778c47
Anton Mak <Anton3841@yandex.ru>
2022-09-06 20:14:00 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundery from "../errorBoundary/ErrorBoundary";
+
+import PropTypes from 'prop-types';
+
+
+import decoration from "../../resources/img/vision.png";
+
+class App extends Component {
+  state = {
+    selectedChar: null,
+  };
+
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppHeader />
+        <main>
+          <ErrorBoundery>
+            <RandomChar />
+          </ErrorBoundery>
+          <div className="char__content">
+            <ErrorBoundery>
+              <CharList onCharSelected={this.onCharSelected} />
+            </ErrorBoundery>
+            <ErrorBoundery>
+              <CharInfo charId={this.state.selectedChar} />
+            </ErrorBoundery>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
